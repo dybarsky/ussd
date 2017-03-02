063a1fd1a47ef523117ab8d89dc4a30fe4e36c94
Maksym Dybarskyi <maksym.dybarskyi@tooploox.com>
2017-03-02 17:35:15 +0100

    Implement accessibility service

--- src/main/java/com/tooploox/ussd/accessibility/UssdAccessibilityService.java
+++ src/main/java/com/tooploox/ussd/accessibility/UssdAccessibilityService.java
@@ -1,13 +1,28 @@
 package com.tooploox.ussd.accessibility;
 
 import android.accessibilityservice.AccessibilityService;
+import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.tooploox.ussd.App;
+import com.tooploox.ussd.data.UssdRepository;
+import com.tooploox.ussd.domain.Ussd;
+
+import java.util.List;
 
 public class UssdAccessibilityService extends AccessibilityService {
 
+    @SuppressLint("NewApi")
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
+        AccessibilityNodeInfo rootNode = event.getSource();
+        List<AccessibilityNodeInfo> ussdResponseTextViewNodesList = rootNode.findAccessibilityNodeInfosByViewId("android:id/message");
+        if (!ussdResponseTextViewNodesList.isEmpty()) {
+            CharSequence ussdReponse = ussdResponseTextViewNodesList.get(0).getText();
+            App.INSTANCE.ussdExecutor.setResult(ussdReponse.toString());
+        }
     }
 
     @Override
